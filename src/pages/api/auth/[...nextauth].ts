14432ced9aac5aa0815378fc02bddf99038d7d4f
Faiz Rahmadani <fzrahmadan@gmail.com>
2024-08-28 21:30:15 +0700

    fixing auth

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -2,19 +2,21 @@ import { signIn } from "@/lib/firebase/service";
 import { compare } from "bcrypt";
 import { NextAuthOptions } from "next-auth";
 import NextAuth from "next-auth/next";
-import CreadentialsProvider from "next-auth/providers/credentials";
+import CredentialsProvider from "next-auth/providers/credentials";
+
 interface User {
   email: string;
-  fullname: string; // add this property
+  fullname: string;
   role: string;
 }
+
 const authOptions: NextAuthOptions = {
   session: {
     strategy: "jwt",
   },
   secret: process.env.NEXTAUTH_SECRET,
   providers: [
-    CreadentialsProvider({
+    CredentialsProvider({
       type: "credentials",
       name: "Credentials",
       credentials: {
@@ -43,15 +45,15 @@ const authOptions: NextAuthOptions = {
   callbacks: {
     jwt({ token, account, profile, user }) {
       if (account?.provider === "credentials" && user) {
-        token.email = user.email;
-        token.fullname = user.fullname;
-        token.role = user.role;
+        // Assert the type of `user` to `User`
+        const typedUser = user as unknown as User;
+        token.email = typedUser.email;
+        token.fullname = typedUser.fullname;
+        token.role = typedUser.role;
       }
-      // console.log({ token, account, user });
       return token;
     },
     async session({ session, token }: any) {
-      //
       if ("email" in token) {
         session.user.email = token.email;
       }
@@ -61,11 +63,9 @@ const authOptions: NextAuthOptions = {
       if ("role" in token) {
         session.user.role = token.role;
       }
-      // console.log(session, token);
       return session;
     },
   },
-  // pages: untuk memberi tahu bahwa halaman ini yang ditunjukan jika ingin login
   pages: {
     signIn: "/auth/login",
   },
